9752a4c94e503d68760258cef654aa68ae915c75
d1gga1 <102676404+d1gga1@users.noreply.github.com>
2023-09-19 20:40:10 +0000

    finished to work with uploading data , i found a new way , without using databse that was not usefull in our case

---
+++ php/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', '','dajboctechdon');
+
+$txtName = $_POST['txtName'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
+?>
