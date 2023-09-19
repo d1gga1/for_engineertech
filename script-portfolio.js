1b4d47c1def503bdf3f95aa7497402859cf9d5e1
IdkMyName187 <140087139+IdkMyName187@users.noreply.github.com>
2023-09-19 17:48:52 +0000

    added portfolio idea, js missing. fixed all navbars to link to respective language

---
+++ script-portfolio.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const toggleButtons = document.querySelectorAll('expand-button');
+    const textContainers = document.querySelectorAll('portfolio-text');
+    const hiddenDivs = document.querySelectorAll('portfolio-hidden-text');
+
+   
+});
