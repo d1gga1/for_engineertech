514a955a93c1b6312ada56740111d2e77827eb31
IdkMyName187 <140087139+IdkMyName187@users.noreply.github.com>
2023-09-21 11:09:45 +0000

    working on portfolio css, finished portfolio css

--- script-portfolio.js
+++ script-portfolio.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const toggleButtons = document.querySelectorAll('expand-button');
-    const textContainers = document.querySelectorAll('portfolio-text');
-    const hiddenDivs = document.querySelectorAll('portfolio-hidden-text');
-
-   
-});
+const toggleButtons = document.querySelectorAll("expand-button");
+const hiddenDiv = document.querySelectorAll("portfolio-hidden-text");
+const 
