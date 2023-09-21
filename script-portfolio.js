c666a2e0aa4bf98b8f3bbccfabcb342e55a32833
IdkMyName187 <140087139+IdkMyName187@users.noreply.github.com>
2023-09-21 17:43:20 +0000

    tried portfolio dropdown js but failed :(

--- script-portfolio.js
+++ script-portfolio.js
@@ -1,3 +1,9 @@
-const toggleButtons = document.querySelectorAll("expand-button");
-const hiddenDiv = document.querySelectorAll("portfolio-hidden-text");
-const 
+const toggleButtons = document.querySelectorAll('expand-button');
+const hiddenDiv = document.querySelectorAll('portfolio-hidden-text');
+const textDiv = document.querySelectorAll('portfolio-text');
+
+toggleButtons.forEach((button, index)=> {
+    button.addEventListener("click", function() {
+        hiddenDiv[index].classList.toggle("show-div");
+    });
+});
