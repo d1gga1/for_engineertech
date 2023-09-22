1d6667b57a3d71e9da353e457536794223eb339a
IdkMyName187 <140087139+IdkMyName187@users.noreply.github.com>
2023-09-22 09:01:37 +0000

    finished portfolio

--- script-portfolio.js
+++ script-portfolio.js
@@ -1,9 +1,11 @@
-const toggleButtons = document.querySelectorAll('expand-button');
-const hiddenDiv = document.querySelectorAll('portfolio-hidden-text');
-const textDiv = document.querySelectorAll('portfolio-text');
+const toggleButtons = document.querySelectorAll('.expand-button');
+const hiddenDiv = document.querySelectorAll('.portfolio-hidden-text');
+const textDiv = document.querySelectorAll('.portfolio-text');
 
 toggleButtons.forEach((button, index)=> {
     button.addEventListener("click", function() {
         hiddenDiv[index].classList.toggle("show-div");
+        button.classList.toggle("rotate");
+        textDiv[index].classList.toggle("corners");
     });
 });
